58465ed01dddc5537476d089720147e173ecea9b
25081997pc <chavanpooja2508@gmail.com>
2022-07-14 17:10:22 +0530

    Checking Employee Present or Absent

--- Program.cs
+++ Program.cs
@@ -5,8 +5,20 @@
         //The method is checking the Employee is Present or Absent
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Management Program");
-        }
-            
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empChk = random.Next(2);
+
+            if (empChk == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }     
     }
 }
