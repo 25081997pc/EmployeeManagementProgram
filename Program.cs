04494e5906bc9e745abc472a7e6b8214b447b6cf
25081997pc <chavanpooja2508@gmail.com>
2022-07-14 17:14:29 +0530

    greeting message

--- Program.cs
+++ Program.cs
@@ -2,11 +2,9 @@
 {
     class Program
     {
-        //The method is checking the Employee is Present or Absent
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Management Program");
-        }
-            
+        }     
     }
 }
