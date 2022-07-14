0c34ee29e5cb46c1ccf8535ccf04d90c32c6b02c
25081997pc <chavanpooja2508@gmail.com>
2022-07-14 17:01:30 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagement
+{
+    class Program
+    {
+        //The method is checking the Employee is Present or Absent
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Management Program");
+        }
+            
+    }
+}
