d1106bd7ba216f24a15c36a851a29a1f923284d8
25081997pc <chavanpooja2508@gmail.com>
2022-07-14 17:36:41 +0530

    Calculating Employee Wage

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace EmployeeManagement
+﻿using System;
+namespace EmployeeManagement
 {
     class Program
     {
+        //The method is calculating daily Employee Wage
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Management Program");
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empChk = random.Next(2);
+
+            if (empChk == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("empWage : " + empWage);
         }     
     }
 }
