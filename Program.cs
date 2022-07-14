783cae3fd6c7fc04fc4f2bd65b424e471792241f
25081997pc <chavanpooja2508@gmail.com>
2022-07-14 18:06:35 +0530

    Using Switch Case Statement

--- Program.cs
+++ Program.cs
@@ -3,32 +3,31 @@ namespace EmployeeManagement
 {
     class Program
     {
-        //The method is Adding Part Time Employee Wage
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
             int empChk = random.Next(3);
 
-            if (empChk == IS_FULL_TIME)
+            switch (empChk)
             {
-                empHrs = 8;
-            }
-            else if (empChk == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("empWage :" + empWage);
+            Console.WriteLine("Emp Wage :" + empWage);
         }     
     }
 }
