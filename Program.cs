f7397c1dac36b563bf2bb5133125d088c62598f8
25081997pc <chavanpooja2508@gmail.com>
2022-07-14 18:33:03 +0530

    Calculating Wages till condition of MaximumHours

--- Program.cs
+++ Program.cs
@@ -7,16 +7,20 @@ namespace EmployeeManagement
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
-            {
-                Random random = new Random();
-                int empChk = random.Next(2);
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empChk = random.Next(3);
                 switch (empChk)
                 {
                     case IS_FULL_TIME:
@@ -29,11 +33,11 @@ namespace EmployeeManagement
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage :" + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day : " + totalWorkingDays + " Emp Hrs : " + empHrs);
             }
-            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }     
     }
 }
